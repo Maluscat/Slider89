dc71bc69b5139e489dfe33590257644738864d74
Hallo89 <hallo89@protonmail.com>
2023-02-04 00:32:48 +0100

    Refactor p4: Add new class `Base` <- Error

---
+++ src/Slider89Base.js
@@ -0,0 +1,45 @@
+'use strict';
+import Slider89Error from './Slider89Error.js';
+
+export default class Slider89Base extends Slider89Error {
+  methods;
+  properties;
+
+  vals = {}; // holding every class property
+  initial = false;
+
+  constructor() {
+    super();
+
+    /* Special properties which are only to be accessed by a getter/setter, never directly:
+     *   $: Fixed endpoint for the values of all properties
+     *   $intermediateThis: Intermediate property (between this and vals) for the keys of an array/object
+     *   $intermediateVals: Intermediate property (between vals and vals.$) for the keys of an array/object
+     *
+     * This results in the following getter/setter paths:
+     *   Normal (primitive & shallow) properties:
+     *     <this.property>   --- Type check & Custom getter/setter --->
+     *     <vals.property>   --- Internal property update --->
+     *     <vals.$.property>
+     *   Deeply defined Arrays:
+     *     <this.property>                        --- Type check & Custom getter/setter --->
+     *     <vals.$intermediateThis.property = []> --- Custom getter/setter on the keys/indexes --->
+     *     <vals.property>                        --- Internal property update --->
+     *     <vals.$intermediateVals.property = []> --- Internal property[key/index] update --->
+     *     <vals.$.property>
+     *
+     * Object.defineProperties is used for non-enumerability & non-writability of these special properties.
+     */
+    Object.defineProperties(this.vals, {
+      '$': {
+        value: {}
+      },
+      '$intermediateThis': {
+        value: {}
+      },
+      '$intermediateVals': {
+        value: {}
+      },
+    });
+  }
+}
