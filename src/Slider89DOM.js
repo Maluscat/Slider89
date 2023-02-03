7a7142436ed0ec71eff7c527418162c4fda8423a
Hallo89 <hallo89@protonmail.com>
2023-02-04 00:34:05 +0100

    Refactor p7: Add new class `DOM` <- Properties

---
+++ src/Slider89DOM.js
@@ -0,0 +1,245 @@
+'use strict';
+import Slider89Properties from './Slider89Properties.js';
+
+export default class Slider89DOM extends Slider89Properties {
+  activeThumb;
+  activeTouchID;
+  mouseDownPos;
+
+  trackStyle;
+
+
+  constructor() {
+    super();
+
+    this.touchStart = this.touchStart.bind(this);
+    this.touchMove = this.touchMove.bind(this);
+    this.touchEnd = this.touchEnd.bind(this);
+
+    this.slideStart = this.slideStart.bind(this);
+    this.slideMove = this.slideMove.bind(this);
+    this.slideEnd = this.slideEnd.bind(this);
+
+    this.keyDown = this.keyDown.bind(this);
+
+    window.addEventListener('keydown', this.keyDown);
+  }
+
+
+  // ---- DOM getters ----
+  getTrackPadding(direction) {
+    return parseFloat(this.trackStyle['padding' + direction]);
+  }
+  getDistance(thumb) {
+    if (this.vals.orientation === 'vertical') {
+      return thumb.getBoundingClientRect().top - this.vals.node.track.getBoundingClientRect().top -
+        this.getTrackPadding('Top');
+    } else {
+      return thumb.getBoundingClientRect().left - this.vals.node.track.getBoundingClientRect().left -
+        this.getTrackPadding('Left');
+    }
+  }
+  getAbsoluteTrackSize(thumb) {
+    if (this.vals.orientation === 'vertical') {
+      return this.vals.node.track.getBoundingClientRect().height - this.getTrackPadding('Top') - this.getTrackPadding('Bottom') -
+        thumb.getBoundingClientRect().height;
+    } else {
+      return this.vals.node.track.getBoundingClientRect().width - this.getTrackPadding('Left') - this.getTrackPadding('Right') -
+        thumb.getBoundingClientRect().width;
+    }
+  }
+
+  computeDistanceValue(thumb, distance, absSize) {
+    if (absSize == null) absSize = this.getAbsoluteTrackSize(thumb);
+    return distance / absSize * (this.vals.range[1] - this.vals.range[0]) + this.vals.range[0];
+  }
+  moveThumb(thumb, distance, useTransform) {
+    if (useTransform) {
+      thumb.style.transform = 'translate' + (this.vals.orientation === 'vertical' ? 'Y' : 'X') + '(' + distance + 'px)';
+    } else {
+      if (this.vals.orientation === 'vertical') {
+        var paddingStart = this.getTrackPadding('Top');
+        var paddingEnd = this.getTrackPadding('Bottom');
+        var thumbDim = thumb.clientHeight;
+        var posAnchor = 'top';
+      } else {
+        var paddingStart = this.getTrackPadding('Left');
+        var paddingEnd = this.getTrackPadding('Right');
+        var thumbDim = thumb.clientWidth;
+        var posAnchor = 'left';
+      }
+
+      let subtract = (thumbDim * distance) + 'px';
+      if (paddingEnd) subtract += ' - ' + (paddingEnd * distance) + 'px';
+      if (paddingStart) subtract += ' + ' + (paddingStart * (1 - distance)) + 'px';
+      thumb.style[posAnchor] = 'calc(' + (distance * 100) + '% - ' + subtract + ')';
+    }
+  }
+
+
+  // ---- Distance computation ----
+  computeAllRatioDistances(newVals, returnProperties) {
+    for (let i = 0; i < this.vals.values.length; i++) {
+      this.computeOneRatioDistance(i, newVals, returnProperties);
+    }
+  }
+  computeOneRatioDistance(thumbIndex, newVals, returnProperties) {
+    let value, ratio;
+    if (!newVals) {
+      newVals = this.vals;
+      value = this.vals.values[thumbIndex];
+    } else {
+      const props = ['range', 'step'];
+      for (let i in props) {
+        if (newVals[props[i]] == null) newVals[props[i]] = this.vals[props[i]];
+      }
+      if (newVals.value != null) {
+        value = newVals.value;
+      } else {
+        ratio = (this.vals.values[thumbIndex] - this.vals.range[0]) / (this.vals.range[1] - this.vals.range[0]);
+        value = (newVals.range[1] - newVals.range[0]) * ratio + newVals.range[0];
+      }
+    }
+    // Round value to a given step
+    if (newVals.step !== false) {
+      if (Math.abs(newVals.range[1] - newVals.range[0]) < newVals.step) {
+        value = newVals.range[0];
+      } else {
+        value = newVals.range[0] + Math.round((value - newVals.range[0]) / newVals.step) * newVals.step;
+      }
+    }
+    const newRatio = (value - newVals.range[0]) / (newVals.range[1] - newVals.range[0]);
+
+    if (returnProperties) {
+      return {
+        value: value,
+        ratio: newRatio
+      };
+    } else {
+      if (value !== this.vals.values[thumbIndex]) this.vals.values[thumbIndex] = value;
+      if (newRatio !== ratio) this.moveThumb(this.vals.node.thumb[thumbIndex], newRatio);
+    }
+  }
+
+
+  // ---- Touch events ----
+  touchStart(e) {
+    e.preventDefault();
+    if (this.activeTouchID == null) {
+      this.activeTouchID = e.changedTouches[0].identifier;
+      this.slideStart.call(this, e.changedTouches[0], e);
+
+      this.vals.node.thumb.addEventListener('touchmove', this.touchMove);
+      this.vals.node.thumb.addEventListener('touchend', this.touchEnd);
+      this.vals.node.thumb.addEventListener('touchcancel', this.touchEnd);
+    }
+  }
+  touchMove(e) {
+    e.preventDefault();
+    for (let i = 0; i < e.changedTouches.length; i++) {
+      if (e.changedTouches[i].identifier === this.activeTouchID) {
+        this.slideMove.call(this, e.changedTouches[i], e);
+        break;
+      }
+    }
+  }
+  touchEnd(e) {
+    e.preventDefault();
+    for (let i = 0; i < e.changedTouches.length; i++) {
+      if (e.changedTouches[i].identifier === this.activeTouchID) {
+        this.vals.node.thumb.removeEventListener('touchmove', this.touchMove);
+        this.vals.node.thumb.removeEventListener('touchend', this.touchEnd);
+        this.vals.node.thumb.removeEventListener('touchcancel', this.touchEnd);
+
+        this.slideEnd.call(this, e.changedTouches[i], e);
+        this.activeTouchID = null;
+        break;
+      }
+    }
+  }
+
+
+  // ---- Mouse events ----
+  slideStart(e, touchEvent) {
+    this.activeThumb = e.currentTarget;
+
+    document.body.classList.add('sl89-noselect');
+    this.activeThumb.classList.add('active');
+    // invokeEvent(['start'], touchEvent || e);
+
+    if (this.vals.orientation === 'vertical') {
+      var posAnchor = 'top';
+      var clientDim = e.clientY;
+    } else {
+      var posAnchor = 'left';
+      var clientDim = e.clientX;
+    }
+    const distance = this.getDistance(this.activeThumb);
+    this.mouseDownPos = clientDim - distance;
+    this.moveThumb(this.activeThumb, distance, true);
+    this.activeThumb.style.removeProperty(posAnchor);
+
+    if (!touchEvent) {
+      window.addEventListener('mouseup', this.slideEnd);
+      window.addEventListener('mousemove', this.slideMove);
+    }
+  }
+  slideMove(e, touchEvent) {
+    const thumbIndex = this.vals.node.thumb.indexOf(this.activeThumb);
+    const absSize = this.getAbsoluteTrackSize(this.activeThumb);
+    let distance = (this.vals.orientation === 'vertical' ? e.clientY : e.clientX) - this.mouseDownPos;
+
+    if (distance > absSize) distance = absSize;
+    else if (distance < 0) distance = 0;
+
+    let value = this.computeDistanceValue(this.activeThumb, distance, absSize);
+    if (this.vals.step !== false) {
+      const computedProperties = this.computeOneRatioDistance(thumbIndex, {value: value}, true);
+      value = computedProperties.value;
+      distance = computedProperties.ratio * absSize;
+    }
+
+    if (this.vals.values[thumbIndex] !== value) {
+      this.vals.values[thumbIndex] = value;
+      this.moveThumb(this.activeThumb, distance, true);
+      this.invokeEvent(['move'], touchEvent || e);
+    }
+  }
+  slideEnd(e, touchEvent) {
+    if (!touchEvent) {
+      window.removeEventListener('mouseup', this.slideEnd);
+      window.removeEventListener('mousemove', this.slideMove);
+    }
+
+    const value = this.computeDistanceValue(this.activeThumb, this.getDistance(this.activeThumb));
+    this.computeOneRatioDistance(this.vals.node.thumb.indexOf(this.activeThumb), {value: value});
+    this.activeThumb.style.removeProperty('transform');
+
+    this.invokeEvent(['end'], touchEvent || e);
+    this.activeThumb.classList.remove('active');
+    document.body.classList.remove('sl89-noselect');
+
+    this.mouseDownPos = null;
+    this.activeThumb = null;
+  }
+
+
+  // ---- Misc events ----
+  keyDown(e) {
+    if (document.activeElement === this.vals.node.thumb) {
+      let modifier = Math.round((this.vals.range[1] - this.vals.range[0]) / 100);
+      if (e.shiftKey && e.ctrlKey) {
+        modifier *= 0.1;
+      } else if (e.shiftKey) {
+        modifier *= 10;
+      }
+      if (e.key === 'ArrowLeft' || e.key === 'ArrowUp') {
+        e.preventDefault();
+        this.value -= modifier;
+      } else if (e.key === 'ArrowRight' || e.key === 'ArrowDown') {
+        e.preventDefault();
+        this.value += modifier;
+      }
+    }
+  }
+}
