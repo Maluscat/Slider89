a2b9639ef76a2b56136d587a83bcafd1835e3cfe
Hallo89 <hallo89@protonmail.com>
2023-02-11 18:41:41 +0100

    Move some static variables to `Slider89Base`

--- src/Slider89Base.js
+++ src/Slider89Base.js
@@ -1,7 +1,171 @@
 'use strict';
 import Slider89Error from './Slider89Error.js';
+import LibraryTypeCheck from './LibraryTypeCheck.js';
 
 export default class Slider89Base extends Slider89Error {
+  static TypeCheck = LibraryTypeCheck;
+
+  static methodStructure = {
+    addEvent: {
+      args: [
+        {
+          name: 'event type',
+          structure: [{
+            type: 'string'
+          }]
+        },
+        {
+          name: 'event function',
+          structure: [{
+            type: 'function'
+          }]
+        },
+        {
+          name: 'event namespace',
+          optional: true,
+          structure: [{
+            type: 'string',
+            conditions: {
+              filled: true,
+              wordChar: true
+            }
+          }]
+        }
+      ]
+    },
+    removeEvent: {
+      args: [
+        {
+          name: 'event identifier/namespace',
+          structure: [
+            {
+              type: 'number',
+              conditions: {
+                nonnegative: true,
+                integer: true
+              }
+            },
+            {
+              type: 'string',
+              conditions: {
+                filled: true,
+                wordChar: true
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+  static propertyStructure = {
+    range: {
+      structure: [
+        {
+          type: 'array',
+          conditions: {
+            length: 2
+          },
+          structure: [
+            { type: 'number' }
+          ]
+        },
+        { type: 'boolean' }
+      ],
+      shape: '[startValue, endValue]',
+    },
+    values: {
+      structure: [{
+        type: 'array',
+        // TODO condition: at least of size 1
+        structure: [{
+          type: 'number'
+        }]
+      }],
+    },
+    value: {
+      structure: [{
+        type: 'number'
+      }],
+    },
+    precision: {
+      structure: [
+        {
+          type: 'number',
+          conditions: {
+            nonnegative: true,
+            integer: true
+          }
+        },
+        { type: 'false' }
+      ],
+    },
+    step: {
+      structure: [
+        {
+          type: 'number',
+          conditions: {
+            positive: true
+          }
+        },
+        { type: 'false' }
+      ],
+    },
+    structure: {
+      structure: [
+        {
+          type: 'string',
+          conditions: {
+            filled: true
+          }
+        },
+        { type: 'false' }
+      ],
+    },
+    node: {
+      default: {},
+    },
+    orientation: {
+      structure: [{
+        type: 'string',
+        conditions: {
+          keywords: [
+            'horizontal',
+            'vertical'
+          ]
+        }
+      }],
+    },
+    classList: {
+      structure: [
+        {
+          type: 'object',
+          structure: [{
+            type: 'array',
+            structure: [
+              { type: 'string' }
+            ]
+          }]
+        },
+        { type: 'false' }
+      ],
+      shape: '{nodeName: [...classes]}'
+    },
+    events: {
+      structure: [
+        {
+          type: 'object',
+          structure: [{
+            type: 'array',
+            structure: [{
+              type: 'function'
+            }]
+          }]
+        },
+        { type: 'false' }
+      ],
+    }
+  };
+
   methods;
   properties;
 
