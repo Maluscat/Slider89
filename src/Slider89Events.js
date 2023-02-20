93afb41a8606d0df07e65ce15c417e9ac74b6dd6
Hallo89 <hallo89@protonmail.com>
2023-02-20 02:37:10 +0100

    Improve error messages for less prose, better readability & information

--- src/Slider89Events.js
+++ src/Slider89Events.js
@@ -18,7 +18,9 @@ export default class Slider89Events extends Slider89Base {
   // ---- Class methods ----
   addEvent(type, fn, name) {
     if (!this.checkEventType(type)) {
-      throw new Slider89.Error('the specified type ‘' + type + '’ is not a valid event type. Available types are:' + Slider89.arrayToListString(Slider89Events.eventTypes), 'addEvent');
+      throw new Slider89.Error(
+        'The specified event type ‘' + type + '’ is not valid. Available types are:' + Slider89.arrayToListString(Slider89Events.eventTypes),
+        'addEvent');
     }
 
     if (!Array.isArray(this.vals.events[type])) this.vals.events[type] = new Array();
@@ -73,10 +75,10 @@ export default class Slider89Events extends Slider89Base {
       // Edge case for 'change:$property'
       const customProp = type.slice('change:'.length);
       if (!Object.prototype.hasOwnProperty.call(this.vals, customProp)) {
-        throw new Slider89.Error(
+        const msg =
           "‘" + type + "’ refers to ‘" + customProp + "’, which isn't a recognized property. "
-          + "Check its spelling and be aware that custom properties need to be initialized",
-          'addEvent');
+          + "Check its spelling and be aware that custom properties need to be initialized";
+        throw new Slider89.Error(msg, 'addEvent');
       }
     } else if (Slider89Events.eventTypes.indexOf(type) === -1) return false;
     return true;
